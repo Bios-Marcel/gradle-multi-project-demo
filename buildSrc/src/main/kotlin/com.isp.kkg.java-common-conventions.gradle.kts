37344d6b7a895d8c2371a9c4ce5801ead5660511
Marcel Schramm <marceloschr@googlemail.com>
2021-10-19 14:39:24 +0200

    Use UTF-8 when compiling

--- buildSrc/src/main/kotlin/com.isp.kkg.java-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/com.isp.kkg.java-common-conventions.gradle.kts
@@ -19,6 +19,19 @@ java {
     }
 }
 
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Test> {
+    systemProperty("file.encoding", "UTF-8")
+}
+
+tasks.withType<Javadoc> {
+    options.encoding = "UTF-8"
+}
+
+
 tasks.test {
     useJUnitPlatform()
 }
